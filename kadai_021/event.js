dbb60bbb7d0cdc10b9f23af107d6bc784a45c1e1
pma1120 <160602917+pma1120@users.noreply.github.com>
2024-04-13 17:28:25 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn');
-const text = document.getElementById('text');
-
-setTimeout(() => {
-    const childList = document.createElement('li');
-    childList.textContent = 'ボタンをクリックしました';
-    text.appendChild(childList);
-    text.textContent = 'ボタンをクリックしました';
-}, 2000);
